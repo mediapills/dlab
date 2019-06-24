c0fce036f1dc089891f0d02acd2261f37715222e
Andrew Yatskovets <jtsandrew@gmail.com>
2019-06-25 02:22:42 +0300

    change struxture

---
+++ plugins/dlab_notebooks/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dlab_notebooks',
+    version='0.0.1',
+    author='Apache Software Foundation',
+    author_email='dev@dlab.apache.org',
+    url='http://dlab.apache.org/',
+    description='Self-service, Fail-safe Exploratory Environment for Collaborative Data Science Workflow',
+    packages=find_packages(),
+)
