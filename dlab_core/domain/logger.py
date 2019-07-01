ad6ace56051124e8e0ecec5f984390ee99533b58
Serhiy Pihura <Serhiy_Pihura@epam.com>
2019-07-01 16:21:05 +0300

    Init logger

--- dlab_core/domain/logger.py
+++ dlab_core/domain/logger.py
@@ -21,6 +21,7 @@
 
 import abc
 import six
+import logging
 
 ERROR = 40
 WARNING = 30
@@ -59,3 +60,243 @@ class BaseLogger:
         Delegate an error call to the underlying logger.
         """
         pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class AbstractLoggingLogger:
+    """Abstract Logger implementation."""
+
+    @abc.abstractmethod
+    def log(self, level, msg, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def add_handler(self, handler):
+        pass
+
+    @property
+    @abc.abstractmethod
+    def level(self):
+        pass
+
+    @level.setter
+    @abc.abstractmethod
+    def level(self, level):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class AbstractLogBuilder:
+    """Abstract Logger Builder implementation."""
+
+    @abc.abstractmethod
+    def add_handlers(self):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class AbstractHandler:
+    """Abstract Handler implementation."""
+
+    def emit(self, record):
+        pass
+
+
+class StreamLogBuilder(AbstractLogBuilder):
+    """Stream LogBuilder implementation.
+    Args:
+        logger_name (str): name of the logger
+        level (int): log level
+    """
+
+    def __init__(self, logger_name, level):
+        logging.basicConfig()
+        self._logger = LoggingLogger(
+            logger=logging.getLogger(logger_name)
+        )
+        self._level = LogLevelTransformer.transform(level)
+
+    def get_logger(self):
+        return self._logger
+
+    def set_log_level(self):
+        self._logger.level = self._level
+
+    def add_stream_handler(self):
+        """Add Stream handler to logger
+        Returns:
+            None
+        """
+        handler = logging.StreamHandler()
+        handler.setLevel(self._level)
+        formatter = SysLogFormatter()
+        handler.setFormatter(formatter)
+        self._logger.add_handler(handler)
+
+    def add_handlers(self):
+        self.add_stream_handler()
+
+
+class LoggingLogger(AbstractLoggingLogger):
+    """Logger which used to log messages.
+    Args:
+        logger (Logger): a logger with the specified name
+    """
+
+    def __init__(self, logger):
+        self._logger = logger
+        self._level = logging.NOTSET
+
+    def log(self, level, msg, *args, **kwargs):
+        if self._logger.isEnabledFor(level):
+            msg, kwargs = self.process(msg, kwargs)
+            level = int(level)
+            self._logger.log(level, msg, *args, **kwargs)
+
+    def add_handler(self, handler):
+        handler_exits = False
+
+        for h in self._logger.handlers:
+            if type(h).__name__ == type(handler).__name__:
+                handler_exits = True
+
+        if not handler_exits:
+            self._logger.propagate = False
+            self._logger.addHandler(hdlr=handler)
+
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, level):
+        self._level = level
+        self._logger.setLevel(level)
+
+    def process(self, msg, kwargs):
+        try:
+            extra = kwargs['extra']
+        except KeyError:
+            extra = {}
+
+        kwargs['extra'] = extra
+
+        return msg, kwargs
+
+
+class CustomLoggerHandler(logging.Handler):
+    """Custom logger handler implementation.
+    Args:
+        handler (AbstractHandler): a specified logger handler
+        level (int): level of logging
+    """
+
+    def __init__(self, handler, level=logging.NOTSET):
+        super(CustomLoggerHandler, self).__init__(level=level)
+        self._handler = handler
+
+    def emit(self, record):
+        self._handler.emit(self.formatter.format(record))
+
+
+class SysLogFormatter(logging.Formatter):
+    """Log Formatter which formats messages by RFC 3164 - BSD-syslog protocol."""
+
+    def __init__(self):
+        log_format = "[%(levelname)s] %(asctime)s - %(message)s"
+        super(SysLogFormatter, self).__init__(
+            fmt=log_format
+        )
+
+
+class LogLevelTransformer:
+    """Log level transformer used to transform system error levels to logging levels."""
+
+    @staticmethod
+    def transform(level):
+        if isinstance(level, (int, float)) and level >= 100:
+            level = int(level / 10)
+        elif isinstance(level, str):
+            prop_level = level.upper()
+
+            if hasattr(logging, prop_level):
+                level = getattr(logging, prop_level)
+            else:
+                level = logging.NOTSET
+
+        return level
+
+    @staticmethod
+    def reverse_transform(level):
+        return level * 10
+
+
+class Logger(BaseLogger):
+    """Logger which work with logging.
+    Args:
+        logger (AbstractLoggingLogger): a specified logger which used to log messages
+    """
+
+    def __init__(self, logger):
+        self._logger = logger
+
+    def log(self, level, msg, *args, **kwargs):
+        level = LogLevelTransformer.transform(level)
+        return self._logger.log(
+            level=level,
+            msg=msg,
+            *args,
+            **kwargs
+        )
+
+    def debug(self, msg, *args, **kwargs):
+        return self.log(
+            level=logging.DEBUG,
+            msg=msg,
+            *args,
+            **kwargs
+        )
+
+    def info(self, msg, *args, **kwargs):
+        return self.log(
+            level=logging.INFO,
+            msg=msg,
+            *args,
+            **kwargs
+        )
+
+    def warn(self, msg, *args, **kwargs):
+        return self.log(
+            level=logging.WARNING,
+            msg=msg,
+            *args,
+            **kwargs
+        )
+
+    def err(self, msg, *args, **kwargs):
+        return self.log(
+            level=logging.ERROR,
+            msg=msg,
+            *args,
+            **kwargs
+        )
+
+
+class LoggerDirector:
+    """Logger Director implementation.
+    Args:
+        builder (AbstractLogBuilder): a specified logger builder
+    """
+    def __init__(self, builder):
+        self._builder = builder
+
+    def build(self):
+        self._builder.set_log_level()
+        self._builder.add_handlers()
+
+    @property
+    def logger(self):
+        logger = Logger(
+            logger=self._builder.get_logger()
+        )
+        return logger
---
+++ tests/unit/test_logger.py
@@ -0,0 +1,282 @@
+# *****************************************************************************
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# ******************************************************************************
+import abc
+import six
+import boto3
+import unittest
+from mock import MagicMock, Mock
+from moto import mock_logs
+
+from dlab_core.domain.logger import BaseLogger
+from dlab_core.domain.logger import (
+    AbstractLoggingLogger, StreamLogBuilder, LoggingLogger, CustomLoggerHandler,
+    LogLevelTransformer, Logger, LoggerDirector
+)
+from dlab_core.infrastructure.logger import StreamCloudWatchLog, AWSLogBuilder, CloudWatchHandler
+
+
+class TestStreamLogBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.builder = StreamLogBuilder(
+            logger_name='Test logger',
+            level=10
+        )
+
+    def test_get_logger(self):
+        logger = self.builder.get_logger()
+
+        self.assertIsInstance(logger, AbstractLoggingLogger)
+
+    def test_set_log_level(self):
+        self.builder.set_log_level()
+
+        self.assertEqual(self.builder.get_logger().level, self.builder._level)
+
+    def test_add_stream_handler(self):
+        self.builder.add_stream_handler()
+
+        self.assertEqual(1, len(self.builder.get_logger()._logger.handlers))
+
+    def test_add_handlers(self):
+        self.builder.add_handlers()
+
+        self.assertEqual(1, len(self.builder.get_logger()._logger.handlers))
+
+
+class TestLoggingLogger(unittest.TestCase):
+    TEST_MESSAGE = 'test log message'
+
+    # noinspection PyTypeChecker
+    def setUp(self):
+        self.mock_logger = MagicMock()
+        self.mock_logger.log.return_value = self.TEST_MESSAGE
+        self.mock_logger.isEnabledFor.return_value = True
+        self.mock_logger.hasHandlers.return_value = True
+
+        self.logger = LoggingLogger(logger=self.mock_logger)
+
+    def test_level(self):
+        self.assertEqual(0, self.logger.level)
+
+    def test_set_level(self):
+        self.logger.level = 10
+        self.assertEqual(10, self.logger.level)
+
+    def test_log(self):
+        self.logger.log(10, self.TEST_MESSAGE)
+
+        self.mock_logger.log.assert_called_with(10, self.TEST_MESSAGE, extra={})
+
+    def test_add_handler(self):
+        handler = MagicMock()
+        self.logger.add_handler(handler)
+
+        self.mock_logger.addHandler.assert_called_with(hdlr=handler)
+
+    def test_process(self):
+        self.assertIn(self.TEST_MESSAGE, self.logger.process(self.TEST_MESSAGE, {}))
+
+
+class TestCustomLoggerHandler(unittest.TestCase):
+
+    # noinspection PyTypeChecker
+    def setUp(self):
+        self.client = MagicMock()
+        self.handler = CustomLoggerHandler(self.client)
+        self.handler.formatter = MagicMock()
+
+    def test_emit(self):
+        self.handler.emit('test')
+
+        self.client.emit.assert_called()
+
+
+class TestLogLevelTransformer(unittest.TestCase):
+
+    def test_log_level_transformer(self):
+        level = LogLevelTransformer.transform('DEBUG')
+
+        self.assertEqual(10, level)
+
+
+class TestLogger(unittest.TestCase):
+    TEST_MESSAGE = 'test log message'
+
+    # noinspection PyTypeChecker
+    def setUp(self):
+        self.client = MagicMock()
+        self.logger = Logger(logger=self.client)
+
+    def test_log(self):
+        self.logger.log(40, self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=40, msg=self.TEST_MESSAGE)
+
+    def test_debug(self):
+        self.logger.debug(self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=10, msg=self.TEST_MESSAGE)
+
+    def test_info(self):
+        self.logger.info(self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=20, msg=self.TEST_MESSAGE)
+
+    def test_warn(self):
+        self.logger.warn(self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=30, msg=self.TEST_MESSAGE)
+
+    def test_err(self):
+        self.logger.err(self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=40, msg=self.TEST_MESSAGE)
+
+
+class TestLoggerDirector(unittest.TestCase):
+
+    # noinspection PyTypeChecker
+    def setUp(self):
+        self.builder = MagicMock()
+
+        self.logger_director = LoggerDirector(
+            builder=self.builder
+        )
+
+    def test_build(self):
+        self.logger_director.build()
+
+        self.builder.add_handlers.assert_called()
+
+    def test_logger(self):
+        logger = self.logger_director.logger
+
+        self.assertIsInstance(logger, BaseLogger)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseCloudWatchLoggerTestCase:
+
+    @abc.abstractmethod
+    def test_create_log_stream(self):
+        pass
+
+    @abc.abstractmethod
+    def test_add(self):
+        pass
+
+
+class TestCloudWatchLogger(BaseCloudWatchLoggerTestCase, unittest.TestCase):
+    GROUP = 'stream_log_group'
+    STREAM_NAME = 'function_name'
+
+    def setUp(self):
+        self.client = boto3.client('logs')
+        self.cw_logs = StreamCloudWatchLog(
+            client=self.client,
+            stream_group=self.GROUP,
+            service_name=self.STREAM_NAME
+        )
+
+        self.msg = Mock()
+        self.msg.message = 'test log message'
+        self.msg.timestamp = 123
+
+    @mock_logs
+    def test_create_log_stream(self):
+        self.client.create_log_group(logGroupName=self.GROUP)
+        self.cw_logs.create_log_stream(self.STREAM_NAME)
+
+        response = self.client.describe_log_streams(logGroupName=self.GROUP)
+
+        self.assertEqual(response['logStreams'][0]['logStreamName'], self.STREAM_NAME)
+
+    # noinspection PyTypeChecker
+    @mock_logs
+    def test_add(self):
+        self.client.create_log_group(logGroupName=self.GROUP)
+        self.cw_logs.add(self.msg)
+        response = self.client.get_log_events(
+            logGroupName=self.GROUP,
+            logStreamName=self.STREAM_NAME,
+        )
+        self.assertEqual(response['events'][0]['message'], 'test log message')
+
+
+class TestAWSLogBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.builder = AWSLogBuilder(
+            logger_name='Test logger',
+            level=10,
+            repository=StreamCloudWatchLog(
+                client=boto3.client('logs'),
+                stream_group='test_group',
+                service_name='service_name'
+            ))
+
+    def test_get_logger(self):
+        logger = self.builder.get_logger()
+
+        self.assertIsInstance(logger, AbstractLoggingLogger)
+
+    def test_set_log_level(self):
+        self.builder.set_log_level()
+
+        self.assertEqual(self.builder.get_logger().level, self.builder._level)
+
+    def test_add_cw_handler(self):
+        self.builder.add_cw_handler()
+
+        self.assertEqual(2, len(self.builder.get_logger()._logger.handlers))
+
+    def test_add_handlers(self):
+        self.builder.add_handlers()
+
+        self.assertEqual(2, len(self.builder.get_logger()._logger.handlers))
+
+
+class TestCloudWatchHandler(unittest.TestCase):
+    GROUP = 'stream_log_group'
+    STREAM_NAME = 'function_name'
+    TEST_MESSAGE = 'test log message'
+
+    def setUp(self):
+        self.client = MagicMock()
+        self.client.put_log_events.return_value = self.TEST_MESSAGE
+
+        self.handler = CloudWatchHandler(
+            client=StreamCloudWatchLog(
+                client=self.client,
+                stream_group=self.GROUP,
+                service_name=self.STREAM_NAME
+            ))
+
+        self.msg = Mock()
+        self.msg.message = self.TEST_MESSAGE
+        self.msg.timestamp = 123
+
+    # noinspection PyTypeChecker
+    def test_emit(self):
+        response = self.handler.emit(self.msg)
+
+        self.assertEqual(response, self.TEST_MESSAGE)
