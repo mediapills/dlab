703695b5f45ae4967c105f6e4a5d5fe7e76b6cef
Serhiy Pihura <Serhiy_Pihura@epam.com>
2019-07-02 12:19:42 +0300

    fix tests

--- tests/unit/test_logger.py
+++ tests/unit/test_logger.py
@@ -20,8 +20,9 @@
 # ******************************************************************************
 
 import unittest
+from mock import MagicMock
 
-# from dlab_core.domain.logger import BaseLogger
+from dlab_core.domain.logger import BaseLogger
 from dlab_core.infrastructure.logger import (
     AbstractLoggingLogger, StreamLogBuilder, LoggingLogger, CustomLoggerHandler,
     LogLevelTransformer, Logger, LoggerDirector
@@ -58,53 +59,51 @@ class TestStreamLogBuilder(unittest.TestCase):
 
 
 class TestLoggingLogger(unittest.TestCase):
-    # TEST_MESSAGE = 'test log message'
-    #
-    # # noinspection PyTypeChecker
-    # def setUp(self):
-    #     self.mock_logger = MagicMock()
-    #     self.mock_logger.log.return_value = self.TEST_MESSAGE
-    #     self.mock_logger.isEnabledFor.return_value = True
-    #     self.mock_logger.hasHandlers.return_value = True
-    #
-    #     self.logger = LoggingLogger(logger=self.mock_logger)
-    #
-    # def test_level(self):
-    #     self.assertEqual(0, self.logger.level)
-    #
-    # def test_set_level(self):
-    #     self.logger.level = 10
-    #     self.assertEqual(10, self.logger.level)
-    #
-    # def test_log(self):
-    #     self.logger.log(10, self.TEST_MESSAGE)
-    #
-    #     self.mock_logger.log.assert_called_with(10, self.TEST_MESSAGE, extra={})
-    #
-    # def test_add_handler(self):
-    #     handler = MagicMock()
-    #     self.logger.add_handler(handler)
-    #
-    #     self.mock_logger.addHandler.assert_called_with(hdlr=handler)
-    #
-    # def test_process(self):
-    #     self.assertIn(self.TEST_MESSAGE, self.logger.process(self.TEST_MESSAGE, {}))
-    pass
+    TEST_MESSAGE = 'test log message'
+
+    # noinspection PyTypeChecker
+    def setUp(self):
+        self.mock_logger = MagicMock()
+        self.mock_logger.log.return_value = self.TEST_MESSAGE
+        self.mock_logger.isEnabledFor.return_value = True
+        self.mock_logger.hasHandlers.return_value = True
+
+        self.logger = LoggingLogger(logger=self.mock_logger)
+
+    def test_level(self):
+        self.assertEqual(0, self.logger.level)
+
+    def test_set_level(self):
+        self.logger.level = 10
+        self.assertEqual(10, self.logger.level)
+
+    def test_log(self):
+        self.logger.log(10, self.TEST_MESSAGE)
+
+        self.mock_logger.log.assert_called_with(10, self.TEST_MESSAGE, extra={})
+
+    def test_add_handler(self):
+        handler = MagicMock()
+        self.logger.add_handler(handler)
+
+        self.mock_logger.addHandler.assert_called_with(hdlr=handler)
+
+    def test_process(self):
+        self.assertIn(self.TEST_MESSAGE, self.logger.process(self.TEST_MESSAGE, {}))
 
 
 class TestCustomLoggerHandler(unittest.TestCase):
 
-    # # noinspection PyTypeChecker
-    # def setUp(self):
-    #     self.client = MagicMock()
-    #     self.handler = CustomLoggerHandler(self.client)
-    #     self.handler.formatter = MagicMock()
-    #
-    # def test_emit(self):
-    #     self.handler.emit('test')
-    #
-    #     self.client.emit.assert_called()
-    pass
+    # noinspection PyTypeChecker
+    def setUp(self):
+        self.client = MagicMock()
+        self.handler = CustomLoggerHandler(self.client)
+        self.handler.formatter = MagicMock()
+
+    def test_emit(self):
+        self.handler.emit('test')
+
+        self.client.emit.assert_called()
 
 
 class TestLogLevelTransformer(unittest.TestCase):
@@ -116,58 +115,55 @@ class TestLogLevelTransformer(unittest.TestCase):
 
 
 class TestLogger(unittest.TestCase):
-    # TEST_MESSAGE = 'test log message'
-    #
-    # # noinspection PyTypeChecker
-    # def setUp(self):
-    #     self.client = MagicMock()
-    #     self.logger = Logger(logger=self.client)
-    #
-    # def test_log(self):
-    #     self.logger.log(40, self.TEST_MESSAGE)
-    #
-    #     self.client.log.assert_called_with(level=40, msg=self.TEST_MESSAGE)
-    #
-    # def test_debug(self):
-    #     self.logger.debug(self.TEST_MESSAGE)
-    #
-    #     self.client.log.assert_called_with(level=10, msg=self.TEST_MESSAGE)
-    #
-    # def test_info(self):
-    #     self.logger.info(self.TEST_MESSAGE)
-    #
-    #     self.client.log.assert_called_with(level=20, msg=self.TEST_MESSAGE)
-    #
-    # def test_warn(self):
-    #     self.logger.warn(self.TEST_MESSAGE)
-    #
-    #     self.client.log.assert_called_with(level=30, msg=self.TEST_MESSAGE)
-    #
-    # def test_err(self):
-    #     self.logger.err(self.TEST_MESSAGE)
-    #
-    #     self.client.log.assert_called_with(level=40, msg=self.TEST_MESSAGE)
-    pass
+    TEST_MESSAGE = 'test log message'
+
+    # noinspection PyTypeChecker
+    def setUp(self):
+        self.client = MagicMock()
+        self.logger = Logger(logger=self.client)
+
+    def test_log(self):
+        self.logger.log(40, self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=40, msg=self.TEST_MESSAGE)
+
+    def test_debug(self):
+        self.logger.debug(self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=10, msg=self.TEST_MESSAGE)
+
+    def test_info(self):
+        self.logger.info(self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=20, msg=self.TEST_MESSAGE)
+
+    def test_warn(self):
+        self.logger.warn(self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=30, msg=self.TEST_MESSAGE)
+
+    def test_err(self):
+        self.logger.err(self.TEST_MESSAGE)
+
+        self.client.log.assert_called_with(level=40, msg=self.TEST_MESSAGE)
+
 
 class TestLoggerDirector(unittest.TestCase):
 
-    # # noinspection PyTypeChecker
-    # def setUp(self):
-    #     self.builder = MagicMock()
-    #
-    #     self.logger_director = LoggerDirector(
-    #         builder=self.builder
-    #     )
-    #
-    # def test_build(self):
-    #     self.logger_director.build()
-    #
-    #     self.builder.add_handlers.assert_called()
-    #
-    # def test_logger(self):
-    #     logger = self.logger_director.logger
-    #
-    #     self.assertIsInstance(logger, BaseLogger)
+    # noinspection PyTypeChecker
+    def setUp(self):
+        self.builder = MagicMock()
 
-    pass
+        self.logger_director = LoggerDirector(
+            builder=self.builder
+        )
 
+    def test_build(self):
+        self.logger_director.build()
+
+        self.builder.add_handlers.assert_called()
+
+    def test_logger(self):
+        logger = self.logger_director.logger
+
+        self.assertIsInstance(logger, BaseLogger)
